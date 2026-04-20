8f7acb62c304872336efdf066a836eed84a0d440
Darsh Shah <q35481193@gmail.com>
2026-04-20 08:33:29 +0000

    hello

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+toolbar.fixedDistance = 30
+var layer = PromiseRejectionEvent.activeLayer
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'
+    var colour = '#'
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}
+
 function onMouseMove(event) {
     var path = new Path2D.Circle({
         center: event.middlePoint,
